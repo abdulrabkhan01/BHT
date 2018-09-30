226db0078b53a656d575e6260df6a52ce589c035
abdul <abdul>
2018-09-30 20:14:37 +0530

    adding basic spring boot project

---
+++ src/main/java/com/bht/bhtwebapp/BhtwebappApplication.java
@@ -0,0 +1,12 @@
+package com.bht.bhtwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhtwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhtwebappApplication.class, args);
+	}
+}
